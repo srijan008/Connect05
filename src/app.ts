5fbdd5530206b8d75dac896963f7dd1132c4b77a
Gaurav <65115466+gauravyad86@users.noreply.github.com>
2025-02-12 14:43:11 +0530

    Added land fetch APIs

--- src/app.ts
+++ src/app.ts
@@ -2,14 +2,11 @@ import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
 import authenticateUser from './middlewares/userAuth';
-import { signUp,login } from './controllers/user.controller';
-
+import { signUp, login } from './controllers/user.controller';
 
 const app = express();
 dotenv.config();
 
-
-// Middleware
 app.use(express.json());
 app.use(cors());
 
@@ -17,20 +14,22 @@ const PORT = process.env.PORT || 3000;
 
 app.get('/api', (req, res) => {
     res.send('Server is running');
-})
+});
 
-app.post('/api/user/signUp', signUp)
-app.post('/api/user/login', login)
+app.post('/api/user/signUp', signUp);
+app.post('/api/user/login', login);
 
 import userRoutes from './routes/user.routes';
-app.use('/api/user',authenticateUser,userRoutes);
+app.use('/api/user', authenticateUser, userRoutes);
 
 import trackRoutes from './routes/track.routes';
-app.use('/api/track',authenticateUser, trackRoutes);
+app.use('/api/track', authenticateUser, trackRoutes);
 
 import areaRoutes from './routes/area.routes';
-app.use('/api/area',authenticateUser, areaRoutes);
+app.use('/api/area', authenticateUser, areaRoutes);
 
+import listingRoutes from './routes/listings.routes';
+app.use('/api/listings', listingRoutes);
 
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
